21146a9c3adb30bae7cfbad1e86ef8e63b54a558
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-05 02:11:00 +0200

    add default name property

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import Plugin from '@swup/plugin';
 
 export default class PluginName extends Plugin {
-    constructor(){
-        super();
-        // this gets run when instance is created
-        // and can be used for things that don't need
-        // to wait for swup instance
-    }
+	name = 'PluginName';
 
+	constructor() {
+		super();
+		// this gets run when instance is created
+		// and can be used for things that don't need
+		// to wait for swup instance
+	}
 
-    mount() {
-        // this is executed when swup is enabled with plugin
-        // you can use this.swup here to access swup instance
-        // example: this.swup.on('clickLink', event => console.log(event))
-    }
+	mount() {
+		// this is executed when swup is enabled with plugin
+		// you can use this.swup here to access swup instance
+		// example: this.swup.on('clickLink', event => console.log(event))
+	}
 
-    unmount() {
-        // this is executed when swup with plugin is disabled
-        // you can use this.swup here to access swup instance
-    }
+	unmount() {
+		// this is executed when swup with plugin is disabled
+		// you can use this.swup here to access swup instance
+	}
 }
