31e0147b8849469117639cb79f1c6c26ad116329
moritzebeling <git@moritzebeling.com>
2022-12-31 16:44:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kirby Maintenance Mode Plugin
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+use Kirby\Cms\App as Kirby;
+
+Kirby::plugin('moritzebeling/kirby-maintenance', [
+
+    'options' => [
+        'ignore' => [],
+        'text' => 'This website is currently under maintenance and will be back online soon.'
+    ],
+
+    'hooks' => [
+        'route:before' => function ($route, $path, $method) {
+
+            if( !option('maintenance', false) ){
+                // maintenance mode is off
+                return;
+            }
+
+            $kirby = kirby();
+
+            $urls = $kirby->urls()->toArray();
+            $ignore = array_merge(option('moritzebeling.kirby-maintenance.ignore', []),[
+                'assets',
+                'api',
+                'media',
+                'panel',
+            ]);
+
+            foreach ($ignore as $i) {
+                if( in_array($i,$urls) ){
+                    // map ignored paths to kirby ingredient urls
+                    $i = $urls[$i];
+                }
+                if( str_starts_with( $path, $i ) ){
+                    // path is ignored or reserved by kirby
+                    return;
+                }
+            }
+
+            if( $kirby->user() ){
+                // user is logged in
+                return;
+            }
+            
+            /* @todo
+            - check if there is a page with the slug "maintenance", if yes, display the page
+            - allow pages to be ignored via field
+            */
+
+            echo option('moritzebeling.kirby-maintenance.text');
+            exit;
+        }
+    ]
+
+]);
