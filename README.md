8605ca15eb473fb3268b3911c21c85219f183ce6
moritzebeling <git@moritzebeling.com>
2023-01-22 15:34:03 +0100

    allow switching through panel

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This plugin uses the `route:before` hook to hide the whole website from logged o
 
 Kirby urls like `assets`, `api`, `media`, `panel` are will be ignored.
 
-## Settings
+There are different ways to control the maintenance mode:
+
+## Via option
 
 ```php
 // site/config.php
@@ -17,11 +19,23 @@ return [
 ];
 ```
 
+## Via panel
+
+Add a field `maintenance` to the `site.yml` blueprint to meet the condition `$site->maintenance()->isTrue()`.
+
+Via `$site->maintenance_text()` you could edit the text that would welcome any logged out website visitor.
+
+You can also use one of the prefabricated blueprint parts:
+
+- `tabs/maintenance`
+- `sections/maintenance`
+- `fields/maintenance`
+- `fields/maintenance_text`
+
 ## 🚧 Work in progress
 This plugin is work in progress and not tested accross multiple use cases. Use at own risk. Thank you for any bug reports and pull requests!
 
 **To do**
 - [ ] check if there is a page with the slug `maintenance`, if yes, display that page
-- [ ] allow toggling of maintenance mode via panel system option
 - [ ] allow pages to be ignored via field or blueprint option
 - [ ] publish as composer package and add to plugins repo on getkirby.com
--- index.php
+++ index.php
@@ -2,6 +2,73 @@
 
 use Kirby\Cms\App as Kirby;
 
+class Maintenance {
+
+    private $kirby;
+
+    public function __construct( $kirby = null )
+    {
+        $this->kirby = $kirby ?? kirby();
+    }
+
+    public function isOn(){
+        if( $this->kirby->option('maintenance', false)){
+            return true;
+        }
+        if( $this->kirby->site()->maintenance()->isTrue() ){
+            return true;
+        }
+        return false;
+    }
+
+    public function isOff(){
+        return !$this->isOn();
+    }
+
+    public function shouldPathBeIgnored( $path ){
+        
+        $urls = $this->kirby->urls()->toArray();
+
+        $ignore = array_merge( $this->kirby->option('moritzebeling.kirby-maintenance.ignore', []),[
+            'assets',
+            'api',
+            'media',
+            'panel',
+        ]);
+
+        foreach ($ignore as $i) {
+            if( in_array($i,$urls) ){
+                // map ignored paths to kirby ingredient urls
+                $i = $urls[$i];
+            }
+            if( str_starts_with( $path, $i ) ){
+                // path is ignored or reserved by kirby
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function showPageToLoggedinUser(){
+        return $this->kirby->user() ? true : false;
+    }
+
+    public function setHeaders(){
+        $protocol = $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1' ? 'HTTP/1.1' : 'HTTP/1.0';
+        header( $protocol . ' 503 Service Unavailable', true, 503 );
+    }
+
+    public function message(){
+        if( $this->kirby->site()->maintenance_text()->isNotEmpty() ){
+            echo $this->kirby->site()->maintenance_text();
+            return;
+        }
+        return option('moritzebeling.kirby-maintenance.text');
+    }
+
+}
+
 Kirby::plugin('moritzebeling/kirby-maintenance', [
 
     'options' => [
@@ -9,46 +76,33 @@ Kirby::plugin('moritzebeling/kirby-maintenance', [
         'text' => 'This website is currently under maintenance and will be back online soon.'
     ],
 
+    'blueprints' => [
+        'fields/maintenance' => __DIR__ . '/blueprints/field.yml',
+        'fields/maintenance_text' => __DIR__ . '/blueprints/text.yml',
+        'sections/maintenance' => __DIR__ . '/blueprints/section.yml',
+        'tabs/maintenance' => __DIR__ . '/blueprints/tab.yml',
+    ],
+
     'hooks' => [
         'route:before' => function ($route, $path, $method) {
 
-            if( !option('maintenance', false) ){
-                // maintenance mode is off
+            $maintenance = new Maintenance();
+
+            if( $maintenance->isOff() ){
+                return;
+            }
+            
+            if( $maintenance->shouldPathBeIgnored( $path ) ){
+                return;
+            }
+            
+            if( $maintenance->showPageToLoggedinUser() ){
                 return;
             }
 
-            $kirby = kirby();
-
-            $urls = $kirby->urls()->toArray();
-            $ignore = array_merge(option('moritzebeling.kirby-maintenance.ignore', []),[
-                'assets',
-                'api',
-                'media',
-                'panel',
-            ]);
-
-            foreach ($ignore as $i) {
-                if( in_array($i,$urls) ){
-                    // map ignored paths to kirby ingredient urls
-                    $i = $urls[$i];
-                }
-                if( str_starts_with( $path, $i ) ){
-                    // path is ignored or reserved by kirby
-                    return;
-                }
-            }
-
-            if( $kirby->user() ){
-                // user is logged in
-                return;
-            }
-
-            // send 503 status code
-
-            $protocol = $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1' ? 'HTTP/1.1' : 'HTTP/1.0';
-            header( $protocol . ' 503 Service Unavailable', true, 503 );
-
-            echo option('moritzebeling.kirby-maintenance.text');
+            $maintenance->setHeaders();
+            $maintenance->message();
+            
             exit;
         }
     ]
